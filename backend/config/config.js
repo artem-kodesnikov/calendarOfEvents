e1213f41a8cbe36631d9e232ba9b53716bab8311
Artem <artiguard@gmail.com>
2023-07-27 11:48:18 +0300

    config

--- backend/config/config.js
+++ backend/config/config.js
@@ -14,7 +14,10 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    host: 'qwe',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   }
 };
